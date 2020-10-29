3b4439cbd26fc8ace64e148dfa7bc4da79510d8f
victorcdrdm <victorcrombez@hotmail.com>
2020-10-29 11:30:19 +0100

    creat playlis of the day

--- src/Controller/TrackController.php
+++ src/Controller/TrackController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 
+use App\Model\PlaylistManager;
 use App\Model\TrackManager;
 
 class TrackController extends AbstractController
@@ -21,18 +22,30 @@ class TrackController extends AbstractController
      * @throws \Twig\Error\RuntimeError
      * @throws \Twig\Error\SyntaxError
      */
+
+
+
     public function add()
     {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $playlistManager = new PlaylistManager();
+
+            $today = new \DateTime();
+            $todayFormat = $today->format('Y-m-d');
+            $playlist = $playlistManager->selectPlaylistsByDay($todayFormat);
+            if (!$playlist) {
+                $newPlaylistId = $playlistManager->createPlaylist($todayFormat);
+            }
             $trackManager = new TrackManager();
             $track = [
-                'title'  => $_POST['title'],
-                'artist' => $_POST['artist'],
-                'url'    => $_POST['url'],
+               'title'  => $_POST['title'],
+               'artist' => $_POST['artist'],
+               'url'    => $_POST['url'],
+                'playlist_id' => $playlist ? $playlist['id'] : $newPlaylistId,
             ];
-             $trackManager->insert($track);
+            $trackManager->insert($track);
             header('Location:/Home/index/');
         }
-        return $this->twig->render('/Home/_form.html.twig');
+        return $this->twig->render('/Home/form.html.twig');
     }
 }
